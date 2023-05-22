3366024fa0a64b380bd9a285d8119eeeccd03fda
m0lts <moltontom@outlook.com>
2023-05-22 15:32:45 +0100

    changed document query selector to container

--- functionality/app.js
+++ functionality/app.js
@@ -264,200 +264,200 @@ const driverContainer = document.querySelectorAll('.driver-container');
 
 driverContainer.forEach(container => {
     if (container.classList.contains('verstappen')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 1;
         driverFirstNameElement.textContent = "Max";
         driverSurname.textContent = "Verstappen";
         driverTeam.src = "./images/teams/red-bull.png";
     } else if (container.classList.contains('perez')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 11;
         driverFirstNameElement.textContent = "Sergio";
         driverSurname.textContent = "Perez";
         driverTeam.src = "./images/teams/red-bull.png";
     } else if (container.classList.contains('hamilton')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 44;
         driverFirstNameElement.textContent = "Lewis";
         driverSurname.textContent = "Hamilton";
         driverTeam.src = "./images/teams/mercedes.png";
     } else if (container.classList.contains('russell')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 63;
         driverFirstNameElement.textContent = "George";
         driverSurname.textContent = "Russell";
         driverTeam.src = "./images/teams/mercedes.png";
     } else if (container.classList.contains('leclerc')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 16;
         driverFirstNameElement.textContent = "Charles";
         driverSurname.textContent = "Leclerc";
         driverTeam.src = "./images/teams/ferrari.png";
     } else if (container.classList.contains('sainz')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 55;
         driverFirstNameElement.textContent = "Carlos";
         driverSurname.textContent = "Sainz";
         driverTeam.src = "./images/teams/ferrari.png";
     } else if (container.classList.contains('alonso')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 14;
         driverFirstNameElement.textContent = "Fernando";
         driverSurname.textContent = "Alonso";
         driverTeam.src = "./images/teams/aston.png";
     } else if (container.classList.contains('stroll')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 18;
         driverFirstNameElement.textContent = "Lance";
         driverSurname.textContent = "Stroll";
         driverTeam.src = "./images/teams/aston.png";
     } else if (container.classList.contains('norris')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 4;
         driverFirstNameElement.textContent = "Lando";
         driverSurname.textContent = "Norris";
         driverTeam.src = "./images/teams/mclaren.png";
     } else if (container.classList.contains('piastri')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 81;
         driverFirstNameElement.textContent = "Oscar";
         driverSurname.textContent = "Piastri";
         driverTeam.src = "./images/teams/mclaren.png";
     } else if (container.classList.contains('gasly')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 10;
         driverFirstNameElement.textContent = "Pierre";
         driverSurname.textContent = "Gasly";
         driverTeam.src = "./images/teams/alpine.png";
     } else if (container.classList.contains('ocon')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 31;
         driverFirstNameElement.textContent = "Esteban";
         driverSurname.textContent = "Ocon";
         driverTeam.src = "./images/teams/alpine.png";
     } else if (container.classList.contains('bottas')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 77;
         driverFirstNameElement.textContent = "Valterri";
         driverSurname.textContent = "Bottas";
         driverTeam.src = "./images/teams/alfa-romeo.png";
     } else if (container.classList.contains('zhou')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 24;
         driverFirstNameElement.textContent = "Guanyu";
         driverSurname.textContent = "Zhou";
         driverTeam.src = "./images/teams/alfa-romeo.png";
     } else if (container.classList.contains('devries')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 21;
         driverFirstNameElement.textContent = "Nyck";
         driverSurname.textContent = "de Vries";
         driverTeam.src = "./images/teams/alpha-tauri.png";
     } else if (container.classList.contains('tsunoda')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 22;
         driverFirstNameElement.textContent = "Yuki";
         driverSurname.textContent = "Tsunoda";
         driverTeam.src = "./images/teams/alpha-tauri.png";
     } else if (container.classList.contains('magnussen')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 20;
         driverFirstNameElement.textContent = "Kevin";
         driverSurname.textContent = "Magnussen";
         driverTeam.src = "./images/teams/haas.png";
     } else if (container.classList.contains('hulkenberg')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 27;
         driverFirstNameElement.textContent = "Niko";
         driverSurname.textContent = "Hulkenberg";
         driverTeam.src = "./images/teams/haas.png";
     } else if (container.classList.contains('albon')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 23;
         driverFirstNameElement.textContent = "Alexander";
         driverSurname.textContent = "Albon";
         driverTeam.src = "./images/teams/williams.png";
     } else if (container.classList.contains('sargeant')) {
-        const driverNumberElement = document.querySelector('.driver-number-p');
-        const driverFirstNameElement = document.querySelector('.firstname');
-        const driverSurname = document.querySelector('.surname');
-        const driverTeam = document.querySelector('.team-img');
+        const driverNumberElement = container.querySelector('.driver-number-p');
+        const driverFirstNameElement = container.querySelector('.firstname');
+        const driverSurname = container.querySelector('.surname');
+        const driverTeam = container.querySelector('.team-img');
 
         driverNumberElement.textContent = 2;
         driverFirstNameElement.textContent = "Logan";
