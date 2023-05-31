3ffc246c70bb9c87cedf97e7d6e06c7510e63671
m0lts <moltontom@outlook.com>
2023-05-31 15:57:02 +0100

    troubleshooting

--- functionality/standings.js
+++ functionality/standings.js
@@ -10,43 +10,43 @@ playerListItem.forEach(item => {
 });
 
 
-// Tally up points for each user
+// // Tally up points for each user
 
-function calculatePoints(classNames, totalPointsElement) {
-    const points = document.querySelectorAll(classNames);
-    let pointsSum = 0;
+// function calculatePoints(classNames, totalPointsElement) {
+//     const points = document.querySelectorAll(classNames);
+//     let pointsSum = 0;
   
-    points.forEach(point => {
-      const value = parseFloat(point.textContent);
+//     points.forEach(point => {
+//       const value = parseFloat(point.textContent);
   
-      if (!isNaN(value)) {
-        pointsSum += value;
-      }
-    });
+//       if (!isNaN(value)) {
+//         pointsSum += value;
+//       }
+//     });
   
-    totalPointsElement.innerHTML = pointsSum;
-  }
+//     totalPointsElement.innerHTML = pointsSum;
+//   }
   
-  // OWEN
-  const owenTotalPoints = document.querySelector('.owen-points-total');
-  calculatePoints('.owen-points', owenTotalPoints);
+//   // OWEN
+//   const owenTotalPoints = document.querySelector('.owen-points-total');
+//   calculatePoints('.owen-points', owenTotalPoints);
   
-  // TOM
-  const tomTotalPoints = document.querySelector('.tom-points-total');
-  calculatePoints('.tom-points', tomTotalPoints);
+//   // TOM
+//   const tomTotalPoints = document.querySelector('.tom-points-total');
+//   calculatePoints('.tom-points', tomTotalPoints);
 
-  // TOBY
-  const tobyTotalPoints = document.querySelector('.toby-points-total');
-  calculatePoints('.toby-points', tobyTotalPoints);
+//   // TOBY
+//   const tobyTotalPoints = document.querySelector('.toby-points-total');
+//   calculatePoints('.toby-points', tobyTotalPoints);
 
-  // ALI
-  const aliTotalPoints = document.querySelector('.ali-points-total');
-  calculatePoints('.ali-points', aliTotalPoints);
+//   // ALI
+//   const aliTotalPoints = document.querySelector('.ali-points-total');
+//   calculatePoints('.ali-points', aliTotalPoints);
 
-  // ED
-  const edTotalPoints = document.querySelector('.ed-points-total');
-  calculatePoints('.ed-points', edTotalPoints);
+//   // ED
+//   const edTotalPoints = document.querySelector('.ed-points-total');
+//   calculatePoints('.ed-points', edTotalPoints);
 
-  // TOBY
-  const jackTotalPoints = document.querySelector('.jack-points-total');
-  calculatePoints('.jack-points', jackTotalPoints);
+//   // TOBY
+//   const jackTotalPoints = document.querySelector('.jack-points-total');
+//   calculatePoints('.jack-points', jackTotalPoints);
