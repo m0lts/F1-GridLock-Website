4d8e4f74bc9df461a799d3bb5cf881fee671937a
m0lts <moltontom@outlook.com>
2023-06-20 15:10:52 +0100

    fixed points issue

--- standings.php
+++ standings.php
@@ -566,7 +566,90 @@
                                         $stmt = $conn->prepare("SELECT * FROM predictions WHERE race = :race_value AND user = :user_value");
 
                                         // Get the next race name
-                                        $content = file_get_contents("https://ergast.com/api/f1/current/6/results.json");
+                                        $content = file_get_contents("https://ergast.com/api/f1/current/7/results.json");
+                                        $result = json_decode($content);
+                                        $nextRace = $result->MRData->RaceTable->Races[0]->raceName;
+
+                                        // Bind the search values to the prepared statement
+                                        $userValue = "Ali";
+                                        $raceValue = $nextRace;
+                                        // $raceValue = "Spanish Grand Prix";
+                                        $stmt->bindParam(':user_value', $userValue);
+                                        $stmt->bindParam(':race_value', $raceValue);
+
+                                        $stmt->execute();
+
+                                        // Check if there is at least one row
+                                        if ($stmt->rowCount() > 0) {
+                                            // Fetch the first row as an indexed array
+                                            $row = $stmt->fetch(PDO::FETCH_NUM);
+                                            $predictedTop10 = array_slice($row, 3);
+
+                                            // Get the actual race result and extract top 10 drivers
+                                            $raceResult = $result->MRData->RaceTable->Races[0]->Results;
+                                            $actualTop10 = [];
+                                            foreach ($raceResult as $item) {
+                                                $driverSurname = $item->Driver->familyName;
+                                                $normalisation = normalizer_normalize($driverSurname, Normalizer::FORM_D);
+                                                $normalisation = preg_replace('/[\x{0300}-\x{036f}]/u', '', $normalisation);
+                                                $lowerCase = mb_strtolower($normalisation);
+                                                $actualTop10[] = $lowerCase;
+                                            }
+                                            $actualTop10 = array_slice($actualTop10, 0, -10);
+                                            // $actualTop10 = ['verstappen', 'hamilton', 'russell', 'perez', 'sainz', 'stroll', 'alonso', 'ocon', 'zhou', 'gasly'];
+
+                                            // Points calculation
+                                            $points = 0;
+                                            if ($predictedTop10 === $actualTop10) {
+                                                $points += 10;
+                                            }
+                                            for ($i = 0; $i < count($predictedTop10); $i++) {
+                                                if ($predictedTop10[$i] === $actualTop10[$i]) {
+                                                    $points += 2;
+                                                }
+                                            }
+                                            for ($j = 0; $j < count($predictedTop10); $j++) {
+                                                for ($l = 0; $l < count($actualTop10); $l++) {
+                                                    if ($predictedTop10[$j] === $actualTop10[$l]) {
+                                                        $points += 1;
+                                                    }
+                                                }
+                                            }
+
+                                            // Print points
+                                            echo $points;
+                                        } else {
+                                            // Handle case when no rows are returned
+                                            echo "";
+                                        }
+
+                                    } catch (PDOException $e) {
+                                        echo "Query failed: " . $e->getMessage();
+                                    } catch (Exception $e) {
+                                        echo "Error: " . $e->getMessage();
+                                    }
+                                ?>
+                            </li>
+                            <li class="prev-points ali-points">
+                                <?php
+                                    // Database details
+                                    $host = "localhost";
+                                    $dbname = "u128425984_predictions";
+                                    $username = "u128425984_moltontom";
+                                    $password = "Wilson2000";
+
+                                    try {
+                                        // Create a new PDO instance
+                                        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+                                        
+                                        // Set PDO error mode to exception
+                                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                        
+                                        // Prepare and execute the SQL query
+                                        $stmt = $conn->prepare("SELECT * FROM predictions WHERE race = :race_value AND user = :user_value");
+
+                                        // Get the next race name
+                                        $content = file_get_contents("https://ergast.com/api/f1/current/8/results.json");
                                         $result = json_decode($content);
                                         $nextRace = $result->MRData->RaceTable->Races[0]->raceName;
 
@@ -1156,7 +1239,7 @@
                                         $stmt = $conn->prepare("SELECT * FROM predictions WHERE race = :race_value AND user = :user_value");
 
                                         // Get the next race name
-                                        $content = file_get_contents("https://ergast.com/api/f1/current/6/results.json");
+                                        $content = file_get_contents("https://ergast.com/api/f1/current/7/results.json");
                                         $result = json_decode($content);
                                         $nextRace = $result->MRData->RaceTable->Races[0]->raceName;
 
