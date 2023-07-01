3f12ff3df89cb6539332e520aeb1bd3840a792ee
m0lts <moltontom@outlook.com>
2023-07-01 12:40:30 +0100

    testing database upload

--- process-signup.php
+++ process-signup.php
@@ -29,6 +29,23 @@ $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
-print_r($_POST);
+$sql = "INSERT INTO accounts (name, email, password_hash)
+        VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+                    $_POST["name"],
+                    $_POST["email"],
+                    $password_hash);
+
+$stmt->execute();
+
+
+echo "Signup successful";
 
 ?>
