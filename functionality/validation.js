2709bf62adca9cdc907c3208604b6412180c9cb5
m0lts <moltontom@outlook.com>
2023-07-04 12:44:49 +0100

    client-side validation coded

--- functionality/validation.js
+++ functionality/validation.js
@@ -12,6 +12,18 @@ validator
     },
     {
         rule: 'email'
+    },
+    {
+        validator: (value) => () => {
+            return fetch("validate-email.php?email=" + encodeURIComponent(value))
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                return json.available;
+            });
+        },
+        errorMessage: "Email address already taken."
     }
   ])
   .addField("#password", [
@@ -27,6 +39,9 @@ validator
         validator: (value, fields) => {
             return value === fields["#password"].elem.value;
         },
-        errorMessage: "Passwords should match"
+        errorMessage: "Passwords should match."
     }
   ])
+  .onSuccess((event) => {
+    document.getElementById("signup").submit();
+  })
---
+++ validate-email.php
@@ -0,0 +1,16 @@
+<?php
+// CHECK EMAIL ISNT ALREADY TAKEN IN DATABASE AND RETURN A BOOLEAN VALUE JSON OBJECT (is_available) 
+
+$mysqli = require __DIR__ . "./database.php";
+
+$sql = sprintf("SELECT * FROM accounts
+                WHERE email = '%s'",
+                $mysqli->real_escape_string($_GET["email"]));
+
+$result = $mysqli->query($sql);
+
+$is_available = $result->num_rows === 0;
+
+header("Content-Type: application/json");
+
+echo json_encode(["available => $is_available"]);
