40117498a35f597c164acd980c5070d5304ec75a
m0lts <moltontom@outlook.com>
2023-07-06 20:08:22 +0100

    added id hashtags to DOM selectors

--- functionality/prediction-validation.js
+++ functionality/prediction-validation.js
@@ -1,7 +1,7 @@
 const validator = new JustValidate("#predictions");
 
   validator
-    .addField("p1", [
+    .addField("#p1", [
         {
             rule: 'required',
         },
@@ -12,47 +12,47 @@ const validator = new JustValidate("#predictions");
             errorMessage: "You must select a driver."
         }
     ])
-    .addField("p2", [
+    .addField("#p2", [
         {
             rule: 'required',
         }
     ])
-    .addField("p3", [
+    .addField("#p3", [
         {
             rule: 'required',
         }
     ])
-    .addField("p4", [
+    .addField("#p4", [
         {
             rule: 'required',
         }
     ])
-    .addField("p5", [
+    .addField("#p5", [
         {
             rule: 'required',
         }
     ])
-    .addField("p6", [
+    .addField("#p6", [
         {
             rule: 'required',
         }
     ])
-    .addField("p7", [
+    .addField("#p7", [
         {
             rule: 'required',
         }
     ])
-    .addField("p8", [
+    .addField("#p8", [
         {
             rule: 'required',
         }
     ])
-    .addField("p9", [
+    .addField("#p9", [
         {
             rule: 'required',
         }
     ])
-    .addField("p10", [
+    .addField("p#10", [
         {
             rule: 'required',
         }
