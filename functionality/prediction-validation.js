4a70bf857d8e15f4c3f346d3531043c776045853
m0lts <moltontom@outlook.com>
2023-07-07 13:37:16 +0100

    all validation complete

--- functionality/prediction-validation.js
+++ functionality/prediction-validation.js
@@ -22,52 +22,223 @@ const validator = new JustValidate(document.querySelector("#predictions"));
                     return true;
                 }
             },
-            errorMessage: "You cannot select the same driver twice."
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p2"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p3"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p4"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p5"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p6"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p7"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p8"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p9").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p9"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p10").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .addField(document.querySelector("#p10"), [
         {
             rule: 'required',
+        },
+        {
+            validator: (value) => {
+                if (value === document.querySelector("#p1").value ||
+                    value === document.querySelector("#p2").value ||
+                    value === document.querySelector("#p3").value ||
+                    value === document.querySelector("#p4").value ||
+                    value === document.querySelector("#p5").value ||
+                    value === document.querySelector("#p6").value ||
+                    value === document.querySelector("#p7").value ||
+                    value === document.querySelector("#p8").value ||
+                    value === document.querySelector("#p9").value
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            errorMessage: "You cannot select the same driver more than once."
         }
     ])
     .onSuccess((event) => {
